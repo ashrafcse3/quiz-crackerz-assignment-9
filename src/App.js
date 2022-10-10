6008a819af148a6e1b41ecbefc8946dc1126e6cd
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-10 23:09:01 +0100

    added blogs and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layouts/Main';
+import Topics from './components/Topics/Topics';
+import Statics from './components/Statics/Statics';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statics',
+          element: <Statics></Statics>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        }
+      ]
+    },
+  ]);
+
   return (
     <div>
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            I am from Header
+            <Link to='/'>Topics</Link>
+            <Link to='/statics'>Statics</Link>
+            <Link to='/blogs'>Blogs</Link>
         </div>
     );
 };
