fc9d0bbca8741d58f1ff545d8b971aaff63eaeac
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-11 11:40:37 +0100

    showing correctAnswer is onClick

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ question: { id, question, correctAnswer, options } }) => {
 
     const selectedAnswerHandler = (event) => {
-        console.log(event.target.value);
+        const selectedAnswer = event.target.value;
+        if (selectedAnswer === correctAnswer) {
+            toast("Answer is correct");
+        }
+        else {
+            toast("Sorry! Answer is incorrect");
+        }
+    }
+
+    const showCorrectAnswer = () => {
+        toast(`Answer: ${correctAnswer}`);
     }
 
     return (
         <div onChange={selectedAnswerHandler.bind(this)} className="bg-white m-5 p-5 text-indigo-900 rounded-lg">
             <div className='flex justify-between'>
                 <h3 className='text-3xl'><strong>Q: </strong>{question}</h3>
-                <button>
+                <button onClick={showCorrectAnswer}>
                     <EyeIcon className="h-9 w-9"></EyeIcon>
                 </button>
             </div>
@@ -23,6 +35,19 @@ const Question = ({ question: { id, question, correctAnswer, options } }) => {
                     <label htmlFor={option}>{option}</label>
                 </div>)
             }
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ToastContainer />
         </div>
     );
 };
