0867c78a37c476d4041b09e2e2ff27e1e331ba69
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-11 11:25:17 +0100

    eyeIcon added from heroIcons

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Question = ({ question: { id, question, correctAnswer, options } }) => {
 
@@ -7,15 +8,18 @@ const Question = ({ question: { id, question, correctAnswer, options } }) => {
     }
 
     return (
-        <div onChange={selectedAnswerHandler.bind(this)} className="bg-white m-5">
+        <div onChange={selectedAnswerHandler.bind(this)} className="bg-white m-5 p-5 text-indigo-900 rounded-lg">
             <div className='flex justify-between'>
-                <h3 className='text-3xl'>Q{question}</h3>
-                <button>eye</button>
+                <h3 className='text-3xl'><strong>Q: </strong>{question}</h3>
+                <button>
+                    <EyeIcon className="h-9 w-9"></EyeIcon>
+                </button>
             </div>
             {
                 options.map(option => <div
-                    key={option}>
-                    <input type="radio" value={option} name={question} id={option} />
+                    key={option}
+                    className="p-2">
+                    <input type="radio" value={option} name={question} id={option} className="mr-2" />
                     <label htmlFor={option}>{option}</label>
                 </div>)
             }
